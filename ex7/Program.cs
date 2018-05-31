5a93d8e89e1183515439f32c90ce62b2e0d8ce55
jancuz <jancuz@yandex.ru>
2018-05-31 18:51:43 +0500

    Решение задачи №7

--- ex7/Program.cs
+++ ex7/Program.cs
@@ -8,6 +8,22 @@ namespace ex7
 {
     class Program
     {
+        // получение подмножества (проверка на существование)
+        // сочетание из N по K с повторениями
+        static bool NextSet(ref int[] arr, int n, int m)
+        {
+            int j = m - 1;  // позиция элемента
+            while (j >= 0 && arr[j] == n) j--;
+            if (j < 0) return false;
+            if (arr[j] >= n)
+                j--;
+            arr[j]++;
+            if (j == m - 1) return true;
+            for (int k = j + 1; k < m; k++)
+                arr[k] = arr[j];
+            return true;
+        }
+
         static void Main(string[] args)
         {
         }
